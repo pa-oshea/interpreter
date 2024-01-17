c2a8f76c88665640ff0626d11019bd4f76b7c230
Patrick O'Shea <p14oshea@gmail.com>
2024-01-17 19:32:57 +0000

    Abstract syntax tree (AST)

---
+++ arkham/ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "arkham/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+
+	return ""
+}
+
+type Indentifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Indentifier) expressionNode()      {}
+func (i *Indentifier) TokenLiteral() string { return i.Token.Literal }
+
+type LetStatement struct {
+	Value Expression
+	Name  *Indentifier
+	Token token.Token
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
